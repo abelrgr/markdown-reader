38349b713b9b376b2e9ab26192e97a2218fdc2d0
Abel <abelgalloruiz@gmail.com>
2024-07-26 21:06:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Simple Markdown Reader
+
+<img src="readme_assets/screenshot1.jpg" alt="Screenshot-1" style="width:30rem; height:20rem;" />
+
+## Description
+
+This is a markdown reader app that allows users to view markdown files. It provides a user-friendly interface for reading and navigating through markdown content.
+
+## Features
+
+- **Markdown rendering**: The app renders markdown files into formatted content, making it easy to read and understand.
+- **Navigation**: Users can easily navigate through the markdown file using a table of contents or by scrolling through the content.
+- **Syntax highlighting**: The app highlights the syntax of code blocks within the markdown file, improving readability for developers.
+- **Customization**: Users can customize the app's appearance and behavior according to their preferences.
+
+## Usage
+
+1. Download the compiled app from the dist folder.
+2. Run the app on your local machine.
+3. Open a markdown file using the app.
+4. Read and navigate through the markdown content.
+
+## Development
+
+To run the app locally:
+
+1. Clone the repository: `git clone https://github.com/your-repo.git`
+2. Install dependencies: `npm install`
+3. Start the app: `npx neu run`
+
+Feel free to modify the code and request a pull request if you have any improvements or suggestions.
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements, please submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## TODO
+
+- [ ] Add the dropped files to the recent files list
+- [ ] Add a search feature to search for text within the markdown content
+- [ ] Implement a dark mode theme for the app
+- [ ] Add support for rendering images and other media content in the markdown file
+- [ ] Review the compiled files for the different platforms
---
+++ resources/js/main.js
@@ -0,0 +1,248 @@
+var validExtensions = [
+  "md",
+  "mkd",
+  "mdwn",
+  "mdown",
+  "mdtxt",
+  "mdtext",
+  "markdown",
+  "text"
+];
+
+var recentFiles = [];
+var storage = { recentFiles: [] };
+
+// Function to get the recent files list from the storage
+async function getStorage() {
+  var data;
+  try {
+    var tmp = await Neutralino.storage.getData("recentFiles");
+    data = JSON.parse(tmp);
+  } catch (error) {
+    await Neutralino.storage.setData(
+      "recentFiles",
+      JSON.stringify({ recentFiles: [] })
+    );
+    data = { recentFiles: [] };
+  }
+
+  recentFiles = data.recentFiles;
+
+  var recentFilesListItem = document.getElementById("recentFilesList");
+
+  recentFiles.forEach((file, index) => {
+    var a = document.createElement("a");
+    a.innerHTML = file;
+    a.href = "#";
+    a.onclick = () => openRecentFile(file);
+    recentFilesListItem.appendChild(a);
+  });
+}
+
+// Function to update the recent files list in the storage
+async function setStorage(item) {
+  if (recentFiles.includes(item)) {
+    recentFiles = recentFiles.filter(file => file !== item);
+  }
+
+  recentFiles.unshift(item);
+  recentFiles = recentFiles.slice(0, 5);
+
+  await Neutralino.storage.setData(
+    "recentFiles",
+    JSON.stringify({ recentFiles })
+  );
+}
+
+// Function to set the content of the drop zone to the parsed markdown content
+function setMarkdownContent(content) {
+  var dropZone = document.getElementById("dropZone");
+  var markDown = marked.parse(content);
+  dropZone.innerHTML = markDown;
+}
+
+// Function to open a recent file by reading its content and displaying it in the drop zone
+async function openRecentFile(file) {
+  var fileContent = await Neutralino.filesystem.readFile(file);
+
+  if (fileContent) {
+    setMarkdownContent(fileContent);
+  }
+}
+
+// Function to open a file dialog and read the selected file's content
+async function openFile() {
+  var selectedFile = await Neutralino.os.showOpenDialog("Select a file", {
+    filters: [{ name: "Markdown files", extensions: validExtensions }],
+  });
+
+  if (selectedFile[0]) {
+    var fileContent = await Neutralino.filesystem.readFile(selectedFile[0]);
+
+    if (fileContent) {
+      setMarkdownContent(fileContent);
+      setStorage(selectedFile[0]);
+    }
+  }
+}
+
+// /*
+//     Function to set up a system tray menu with options specific to the window mode.
+//     This function checks if the application is running in window mode, and if so,
+//     it defines the tray menu items and sets up the tray accordingly.
+// */
+function setTray() {
+  // Tray menu is only available in window mode
+  if (NL_MODE != "window") {
+    console.log("INFO: Tray menu is only available in the window mode.");
+    return;
+  }
+
+  // Define tray menu items
+  var tray = {
+    icon: "/resources/icons/trayIcon.png",
+    menuItems: [
+      { id: "VERSION", text: "Get version" },
+      { id: "SEP", text: "-" },
+      { id: "QUIT", text: "Quit" },
+    ],
+  };
+
+  // Set the tray menu
+  Neutralino.os.setTray(tray);
+}
+
+/*
+    Function to handle click events on the tray menu items.
+    This function performs different actions based on the clicked item's ID,
+    such as displaying version information or exiting the application.
+*/
+function onTrayMenuItemClicked(event) {
+  switch (event.detail.id) {
+    case "VERSION":
+      // Display version information
+      Neutralino.os.showMessageBox(
+        "Version information",
+        `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`
+      );
+      break;
+    case "QUIT":
+      // Exit the application
+      Neutralino.app.exit();
+      break;
+  }
+}
+
+// Function to handle the window close event by gracefully exiting the Neutralino application
+function onWindowClose() {
+  Neutralino.app.exit();
+}
+
+// Initialize Neutralino
+Neutralino.init();
+
+// Register event listeners
+Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
+Neutralino.events.on("windowClose", onWindowClose);
+
+// Conditional initialization: Set up system tray if not running on macOS
+if (NL_OS != "Darwin") {
+  // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
+  setTray();
+}
+
+getStorage();
+
+// Add the event listener to the drop zone
+document.addEventListener("DOMContentLoaded", () => {
+  var dropZone = document.getElementById("dropZone");
+
+  // Prevent default drag behaviors
+  ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+    dropZone.addEventListener(eventName, preventDefaults, false);
+  });
+
+  dropZone.addEventListener("click", ($event) => {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    if ($event.target.tagName === "A") {
+      Neutralino.os.open($event.target.href);
+    }
+  });
+
+  function preventDefaults(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  ["dragenter", "dragover"].forEach((eventName) => {
+    dropZone.addEventListener(
+      eventName,
+      () => {
+        dropZone.classList.add("hover");
+      },
+      false
+    );
+  });
+
+  ["dragleave", "drop"].forEach((eventName) => {
+    dropZone.addEventListener(
+      eventName,
+      () => {
+        dropZone.classList.remove("hover");
+      },
+      false
+    );
+  });
+
+  dropZone.addEventListener("drop", handleDrop, false);
+
+  function handleDrop(e) {
+    var dt = e.dataTransfer;
+    var files = dt.files;
+
+    handleFiles(files);
+  }
+
+  function validateFile(file) {
+    var extension = file.name.split(".").pop();
+    return validExtensions.includes(extension);
+  }
+
+  function previewFile(file) {
+    var filePath = file.path;
+    console.log(filePath);
+
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onloadend = () => {
+      var markDown = marked.parse(reader.result);
+      dropZone.innerHTML = markDown;
+    };
+  }
+
+  function handleFiles(files) {
+    files = [...files];
+    files.forEach((file) => {
+      var isValid = validateFile(file);
+      if (isValid) {
+        previewFile(file);
+      }
+    });
+  }
+});
+
+
+
+
+// #region Modal
+var modal = document.getElementById("myModal");
+
+function openAboutModal() {
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
